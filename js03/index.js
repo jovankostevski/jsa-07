ee2db963fd773dac4e61b8de7a3779a55071f852
Jovan Kostevski <jovankostevski@gmail.com>
2020-09-22 22:10:08 +0200

    js03 end

--- js03/index.js
+++ js03/index.js
@@ -32,13 +32,13 @@ const fileWrite = (file, content) => {
     });
 };
 
-fileWrite('text.txt', 'Hello World!')
-    .then(() => {
-        console.log('Promise done!');
-    })
-    .catch(err => {
-        console.error(err);
-});
+// fileWrite('text.txt', 'Hello World!')
+//     .then(() => {
+//         console.log('Promise done!');
+//     })
+//     .catch(err => {
+//         console.error(err);
+// });
 
 const fileAppend = (file, content) => {
     return new Promise((resolve, reject) => {
@@ -51,13 +51,13 @@ const fileAppend = (file, content) => {
     });
 };
 
-fileAppend('text.txt', 'Hello Again!')
-    .then(() => {
-        console.log('Promise 2 done!');
-    })
-    .catch(err => {
-        console.error(err);
-});
+// fileAppend('text.txt', 'Hello Again!')
+//     .then(() => {
+//         console.log('Promise 2 done!');
+//     })
+//     .catch(err => {
+//         console.error(err);
+// });
 
 const fileRead = (file) => {
     return new Promise((resolve, reject) => {
@@ -70,10 +70,46 @@ const fileRead = (file) => {
     });
 };
 
-fileRead('text.txt')
-    .then((data) => {
+// fileRead('text.txt')
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(err => {
+//         console.error(err);
+// });
+
+fileWrite('text.txt', 'Promise chaining...')
+    .then(() => {
+        return fileAppend('text.txt', 'Chain 1');
+    })
+    .then(() => {
+        return fileRead('text.txt');
+    })
+    .then(data => {
         console.log(data);
     })
     .catch(err => {
         console.error(err);
 });
+
+const fileWork = async () => {
+    try {
+        await fileWrite('text.txt', 'Async/await chaining...');
+        await fileAppend('text.txt', 'Append...');
+        let data = await fileRead('text.txt');
+        console.log(data);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+fileWork();
+
+const getConfig = () => {
+    let data = fs.readFileSync('config.json', 'utf8');
+    return JSON.parse(data);
+}
+
+console.log(getConfig().db.dbname);
+console.log(getConfig().mail.key);
+console.log(getConfig().server.port);
